02720328d54af57913ccba412bfbfedff3ff6ff3
ibrahimmuradov <muradovibrahim2412@mgail.com>
2024-10-18 11:35:26 +0400

    update main & del db

--- phish/main.py
+++ phish/main.py
@@ -3,7 +3,10 @@ from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 import os
-from .routers import users, training, email, target, administration, role
+from .routers import users, training, email, target, administration
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+from .routers.email import manager
 
 app = FastAPI()
 
@@ -11,7 +14,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
@@ -31,11 +34,23 @@ async def get_image(image_name: str):
 
 app.mount("/static", StaticFiles(directory=upload_folder), name="static")
 
+
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    print("Client connected")
+    try:
+        while True:
+            message = await websocket.receive_text()
+            print(f"Message received: {message}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        print("Client disconnected")
+
+
 app.include_router(users.router)
 app.include_router(training.router)
 app.include_router(email.router)
 app.include_router(target.router)
 app.include_router(administration.router)
-app.include_router(role.router)
 
 app.include_router(static_router, prefix="/api")
